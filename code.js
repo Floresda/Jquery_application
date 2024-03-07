203301bca8c92584a301544940f0b3d957edae10
Floresda <97864447+Floresda@users.noreply.github.com>
2024-03-07 14:22:14 -0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#name").text("David Flores");
+    $("#major").text("Computer Graphics Technology");
+    $("#year").text("Senior");
+    
+    var homeCountry = "<em>United States</em>";
+    $("#country").html("<strong>" + homeCountry + "</strong>");
+});
