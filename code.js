09cb1c4d5f9564f3f2c3babaa0ecf451955c5730
Floresda <97864447+Floresda@users.noreply.github.com>
2024-03-07 13:57:49 -0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('#name').text('John Doe');
+    $('#major').text('Computer Science');
+    $('#year').text('Sophomore');
+    
+    var homeCountry = '<em>Ireland</em>';
+    $('#country').html('<strong>' + homeCountry + '</strong>');
+});
+
+$(document).ready(setUp);
